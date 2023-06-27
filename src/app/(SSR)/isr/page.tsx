574c00e941817a10112f9ae059ead61b55c9ce5a
Lee Rogers <edwinleerogers@gmail.com>
2023-06-27 13:01:01 -0700

    add dynamic and ISR pages

--- src/app/(SSR)/isr/page.tsx
+++ src/app/(SSR)/isr/page.tsx
@@ -2,13 +2,19 @@ import { UnsplashImage } from "@/models/unsplash-image"
 import Image from "next/image"
 import Link from "next/link"
 
-const Static = async () => {
+export const metadata = {
+  title: 'Incremental Static Regeneration Image Gallery',
+}
+
+export const revalidate = 15
+
+export default async function Page() {
   const response = await fetch('https://api.unsplash.com/photos/random?client_id=' + process.env.UNSPLASH_ACCESS_KEY)
   const image: UnsplashImage = await response.json()
   const width = Math.min(500, image.width)
   const height = (width / image.width) * image.height
 
-  return (
+  return(
     <div className="d-flex flex-column align-items-center">
       <Image
         src={image.urls.raw}
@@ -19,7 +25,5 @@ const Static = async () => {
       />
       by <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
     </div>
-  );
+  )
 }
- 
-export default Static;
--- src/app/hello/page.tsx
+++
@@ -1,6 +0,0 @@
-const Hello = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-  return ( <div>HELLLOOOOO</div> );
-}
- 
-export default Hello;
