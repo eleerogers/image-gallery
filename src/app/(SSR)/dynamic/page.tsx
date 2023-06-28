2e6dbde41dd173b8d98ab7434f6e2dc11cafac16
Lee Rogers <edwinleerogers@gmail.com>
2023-06-28 11:34:54 -0700

    alternate way to dynamically render, commented out

--- src/app/(SSR)/dynamic/page.tsx
+++ src/app/(SSR)/dynamic/page.tsx
@@ -9,7 +9,10 @@ export const metadata = {
 export const revalidate = 0
 
 export default async function Page() {
-  const response = await fetch('https://api.unsplash.com/photos/random?client_id=' + process.env.UNSPLASH_ACCESS_KEY)
+  const response = await fetch('https://api.unsplash.com/photos/random?client_id=' + process.env.UNSPLASH_ACCESS_KEY, {
+    // cache: "no-cache"/"no-store"
+    // next: { revalidate: 0 }
+  })
   const image: UnsplashImage = await response.json()
   const width = Math.min(500, image.width)
   const height = (width / image.width) * image.height
--- src/app/(SSR)/isr/page.tsx
+++ src/app/(SSR)/isr/page.tsx
@@ -9,7 +9,10 @@ export const metadata = {
 export const revalidate = 15
 
 export default async function Page() {
-  const response = await fetch('https://api.unsplash.com/photos/random?client_id=' + process.env.UNSPLASH_ACCESS_KEY)
+  const response = await fetch('https://api.unsplash.com/photos/random?client_id=' + process.env.UNSPLASH_ACCESS_KEY,
+  {
+      // next: { revalidate: 15 }
+  })
   const image: UnsplashImage = await response.json()
   const width = Math.min(500, image.width)
   const height = (width / image.width) * image.height
