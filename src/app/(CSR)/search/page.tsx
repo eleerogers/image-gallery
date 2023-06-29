5f56851c91c3655255766a7cdb29aa5f700a50a7
Lee Rogers <edwinleerogers@gmail.com>
2023-06-29 11:26:46 -0700

    add search page using client side routing

---
+++ src/app/(CSR)/search/page.tsx
@@ -0,0 +1,14 @@
+
+import SearchPage from "./SearchPage";
+
+export const metadata = {
+  title: "Search - Image Gallery"
+}
+
+const Page = () => {
+  return (
+    <SearchPage />
+  );
+}
+ 
+export default Page;
